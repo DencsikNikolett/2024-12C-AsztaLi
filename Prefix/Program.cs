4fb321d593f94e1eb0a1c4a55abac37ace4b41f0
Nikolett Dencsik <2021126@dszcberegszaszi.hu>
2024-10-02 09:43:54 +0200

    Prefix

---
+++ Prefix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Console.WriteLine($"Az x eredeti értéke: {x}");
+            Console.WriteLine($"Az x értéke '++x' iratással: {++x}");
+            Console.WriteLine($"Az x értéke kiiratás után: {x}");
+
+            Console.WriteLine($"\nAz y eredeti értéke: {y}");
+            Console.WriteLine($"Az y értéke 'y++' iratással: {y++}");
+            Console.WriteLine($"Az y értéke kiiratás után: {y}");
+            Console.ReadKey();
+
+        }
+    }
+}
