4ed19fd77e55a7f473a27fb21e2f9a119df77884
Nikolett Dencsik <2021126@dszcberegszaszi.hu>
2024-10-02 08:13:52 +0200

    Enum pelda

---
+++ Macskak/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macskak
+{
+    class Program
+    {
+
+        enum Animals { Cat, Dog, Tiger, Wolf};
+
+        static void Main(string[] args)
+        {
+            Animals b = Animals.Wolf;
+            Animals c = Animals.Cat;
+
+            if (b == Animals.Wolf)
+            {
+                Console.WriteLine("A b állat Wolf.");
+            }
+            if (c == Animals.Cat)
+            {
+                Console.WriteLine("A c állat Cat.");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
