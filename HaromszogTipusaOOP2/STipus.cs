30f441798f765857f0211895db59adbc90a4eaf4
Nikolett Dencsik <2021126@dszcberegszaszi.hu>
2024-11-27 10:05:20 +0100

    11.27

---
+++ HaromszogTipusaOOP2/STipus.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaromszogTipusaOOP2
+{
+    class STipus
+    {
+        private int a, b, c;
+
+        public HTipus(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public bool Ervenyesharomszog()
+        {
+            return (a + b > c) && (a + c > b) && (b + c > a);
+        }
+
+        public bool Derekszoguharomszog()
+        {
+            if (!Ervenyesharomszog()) return false;
+            int[] oldalak = {a, b, c };
+            Array.Sort(oldalak);
+            return (oldalak[0] * oldalak[0] + oldalak[1] * oldalak[1] == oldalak[2] * oldalak[2]);
+        }
+
+        public bool EgyenloSzaruHaromszog()
+        {
+            if (!Ervenyesharomszog()) return false;
+            return (a == b) || (a == c) || (b == c);
+        }
+
+        public bool EgyenloOldaluHaromszog()
+        {
+            if (!Ervenyesharomszog()) return false;
+            return (a == b) && (b == c);
+        }
+
+        public double Terulet()
+        {
+            if (!Ervenyesharomszog()) throw new InvalidOperationException("Nem érvényes a háromszög.");
+            double s = (a + b + c) / 2.0;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HTipus haromszog1 - new HTipus(1, 4, 5);
+            Console.WriteLine($"Érvényes háromszög: {haromszog1.ErvenyesHaromszog()}"); 
+            Console.WriteLine($"Érvényes háromszög: {haromszog1.ErvenyesHaromszog()}");
+            Console.WriteLine($"Érvényes háromszög: {haromszog1.ErvenyesHaromszog()}");
+            Console.WriteLine($"Érvényes háromszög: {haromszog1.ErvenyesHaromszog()}");
+            Console.WriteLine($"Érvényes háromszög: {haromszog1.ErvenyesHaromszog()}");
+
+
+        }
+    }
+}
---
+++ Kivetel/KivetelClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class KivetelClass
+    {
+        //Nincs kivétel kezelés
+        public void KivKezNincs()
+        {
+            Console.WriteLine("Nincs kivételkezelés\nAdjon meg egy karaktert!");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A beírt szám: {0}", szam);
+            Console.ReadLine();
+        }
+        //Általános kivitelezés
+        public void KivKezAlt()
+        {
+            Console.WriteLine("\nHiba objektum nélküli kivételkezelés\nAdjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            }
+            catch
+            {
+                Console.WriteLine("Hibás adat");
+                throw;
+            }
+            Console.ReadKey();
+        }
+        //Őshiba objektummal
+        public void KivKezOs()
+        {
+            Console.WriteLine("\nŐs hiba objektummal kivételkezelés\nAdjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
